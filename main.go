6b0a71b9603030f3d12b90c2773b8e4d0c309941
Alex <alex@pop-os.localdomain>
2021-02-21 20:36:36 +1030

    Using urfave cli module

--- main.go
+++ main.go
@@ -1,42 +1,102 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"os"
+
 	"github.com/pkg/errors"
+	"github.com/urfave/cli/v2"
 	TerramlCodeGenerator "github.com/zakufish/terraml/codegen"
 	TerramlCodeRunner "github.com/zakufish/terraml/coderun"
 	TerramlParser "github.com/zakufish/terraml/terramlparser"
 )
 
-func main() {
-	var filePath string
-	var variableFilePath string
-	var action string
-	flag.StringVar(&filePath, "file", "", "Path to file")
-	flag.StringVar(&variableFilePath, "variables", "", "Path to variables file")
-	flag.StringVar(&action, "action", "", "Action item")
+func runAction(c *cli.Context) error {
+	action := c.Command.Name
+	template := c.String("template")
+	variables := c.String("variables")
 
-	flag.Parse()
+	if template == "" {
+		return errors.New("no template file supplied to execute")
+	}
 
-	if filePath == "" {
-		fmt.Println(fmt.Errorf("no file supplied to execute"))
+	if variables == "" {
+		return errors.New("no variables file supplied to execute")
 	}
 
 	fmt.Println("Rendering and generating the deployment manifests ...")
-	deploymentOrder, deploymentManifest, err := TerramlParser.GetDeploymentManifest(filePath, variableFilePath)
+	deploymentOrder, deploymentManifest, err := TerramlParser.GetDeploymentManifest(template, variables)
 	if err != nil {
-		fmt.Println(errors.Wrap(err, "terraml: template rendering error"))
+		return errors.Wrap(err, "terraml: template rendering error")
 	}
 
 	fmt.Println("Generating Terraform code directories ...")
 	executeOrder, err := TerramlCodeGenerator.GenerateTerraformCodeDirectories(deploymentOrder, deploymentManifest)
 	if err != nil {
-		fmt.Println(errors.Wrap(err, "terraml: terraform code generation error"))
+		return errors.Wrap(err, "terraml: terraform code generation error")
 	}
 
 	fmt.Println("Executing Terraform commands ...")
 	if err := TerramlCodeRunner.RunTerraformCode(executeOrder, action); err != nil {
-		fmt.Println(errors.Wrap(err, "terraml: terraform command execution error"))
+		return errors.Wrap(err, "terraml: terraform command execution error")
+	}
+	return nil
+}
+
+func main() {
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:     "template",
+			Aliases:  []string{"t"},
+			Usage:    "Load template from `FILE`",
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "variables",
+			Aliases:  []string{"v"},
+			Usage:    "Load variables from `FILE`",
+			Required: true,
+		},
+	}
+
+	terraml := &cli.App{
+		Name:  "terraml",
+		Usage: "Build complex Terraform through YAML",
+		Commands: []*cli.Command{
+			{
+				Name:    "init",
+				Aliases: []string{"i"},
+				Usage:   "initialize the working directory for Terraform.",
+				Action:  runAction,
+				Flags:   flags,
+			},
+			{
+				Name:    "plan",
+				Aliases: []string{"p"},
+				Usage:   "create an execution plan.",
+				Action:  runAction,
+				Flags:   flags,
+			},
+			{
+				Name:    "apply",
+				Aliases: []string{"a"},
+				Usage:   "apply the changes required to reach the desired state of the configuration.",
+				Action:  runAction,
+				Flags:   flags,
+			},
+			{
+				Name:    "destroy",
+				Aliases: []string{"d"},
+				Usage:   "destroy the Terraform-managed infrastructure.",
+				Action:  runAction,
+				Flags:   flags,
+			},
+		},
+		EnableBashCompletion: true,
+	}
+
+	err := terraml.Run(os.Args)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
 	}
 }
